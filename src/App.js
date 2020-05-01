bc07003405b40da5e8c7fd849d56e25741afeaf4
pushpitgarg <gargpushpit5@gmail.com>
2020-05-01 16:44:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes.js';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-     <Navbar dark color="primary">
-         <NavbarBrand href="/">hello welocme to first tutorial</NavbarBrand>
-      </Navbar>
-         <Menu></Menu>
-       </div>
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
     
-    </div>
-  );
+    return (
+      <div className="App">
+        <div className="container">
+       <Navbar dark color="primary">
+           <NavbarBrand href="/">hello welocme to first tutorial</NavbarBrand>
+        </Navbar>
+           <Menu dishes={this.state.dishes}></Menu>
+         </div>
+      
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
